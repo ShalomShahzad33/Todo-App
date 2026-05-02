e3c9104e9038bf9ce917cc60019bdb174aa8f1e7
ShalomShahzad33 <shalomshahzad450@gmail.com>
2026-05-02 22:51:08 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="bg-gray-950 h-screen text-white flex justify-center items-center">
+      <h1 className="text-4xl">Shalom's Todo App</h1>
+    </main>
+  );
+};
+
+export default App;
